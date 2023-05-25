72623a0c2064f4964c2690d110e6a0d1ab9fe12a
MemikKaratas46 <karatas.memik@iisgalvanimi.edu.it>
2023-05-25 12:47:00 +0000

    commento

---
+++ src/app/artisti/artisti.component.ts
@@ -0,0 +1,18 @@
+
+import { Component, Input } from '@angular/core';
+import { Artista } from 'src/models/streaming.model';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-artisti',
+  templateUrl: './artisti.component.html',
+  styleUrls: ['./artisti.component.css']
+})
+export class ArtistiComponent {
+  @Input() artisti:Artista[];
+  constructor(private router:Router){
+    this.artisti =this.router.getCurrentNavigation().extras.state as Artista[];
+
+  }
+
+}
